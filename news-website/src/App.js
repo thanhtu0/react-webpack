498b83bf0ed9f694c7d3ff81cf50788710a5db68
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-08-15 14:13:45 +0700

    feat: delete Emart project & add News-Website project

---
+++ news-website/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Newsapp from "./Componentss/Newsapp";
+
+function App() {
+    return (
+        <div className="App">
+            <Newsapp />
+        </div>
+    );
+}
+
+export default App;
