d1b0fdf255f9c45e4dd1738341a36d882c692c29
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-08-16 10:19:50 +0700

    feat: implement meal fetching functionality and integrate MealProvider

---
+++ food-app/src/context/mealContext.js
@@ -0,0 +1,31 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { mealReducer } from "../reducers/mealReducer";
+import { startFetchCategories } from "../actions/mealsActions";
+
+const initialState = {
+    categories: [],
+    categoryLoading: false,
+    categoryError: false,
+    categoryMeals: [],
+    categoryMealsLoading: false,
+    categoryMealsError: false,
+};
+
+const MealContext = createContext({});
+export const MealProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(mealReducer, initialState);
+
+    useEffect(() => {
+        startFetchCategories(dispatch);
+    }, []);
+
+    return (
+        <MealContext.Provider value={{ ...state, startFetchCategories }}>
+            {children}
+        </MealContext.Provider>
+    );
+};
+
+export const useMealContext = () => {
+    return useContext(MealContext);
+};
--- food-app/src/utils/constants.js
+++ food-app/src/utils/constants.js
@@ -1,4 +1,1 @@
-import header_image from "../assets/images/header_image.jpg";
-import loader from "../assets/images/loader.svg";
-
-export { header_image, loader };
+export const CATEGORIES_URL = "categories.php";
