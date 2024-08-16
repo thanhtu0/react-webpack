cb147a0c1ac299e0cac5586d191ed1b6c4ab60bf
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-08-16 13:47:15 +0700

    feat: extend meal context and reducer with additional states and actions for fetching meals and single meal data

--- food-app/src/pages/HomePage/HomePage.js
+++ food-app/src/pages/HomePage/HomePage.js
@@ -1,7 +1,21 @@
 import "./HomePage.scss";
+import { useMealContext } from "../../context/mealContext";
+import Loader from "../../components/Loader/Loader";
+import CategoryList from "../../components/Category/CategoryList";
 
 const HomePage = () => {
-    return <main className="main-content"></main>;
+    const { categories, meals, categoryLoading, mealsLoading } =
+        useMealContext();
+    console.log(categories, meals, categoryLoading, mealsLoading);
+    return (
+        <main className="main-content">
+            {categoryLoading ? (
+                <Loader />
+            ) : (
+                <CategoryList categories={categories} />
+            )}
+        </main>
+    );
 };
 
 export default HomePage;
--- food-app/src/utils/constants.js
+++ food-app/src/utils/constants.js
@@ -1,1 +1,5 @@
 export const CATEGORIES_URL = "categories.php";
+
+export const MEAL_CATEGORIES_URL = "filter.php?c=";
+
+export const MEAL_SINGLE_URL = "lookup.php?i=";
