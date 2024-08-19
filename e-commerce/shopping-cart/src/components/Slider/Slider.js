7b3b9f448724cb17a847d93da652466966410f5d
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-08-19 14:24:19 +0700

    feat: update HomePage with API integration and add Category, Slider components

--- e-commerce/shopping-cart/src/components/Slider/Slider.js
+++ e-commerce/shopping-cart/src/components/Slider/Slider.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Slider.scss";
+import { slider_img_1 } from "../../utils/images";
 
 const Slider = () => {
-    return <div>Slider</div>;
+    return (
+        <div className="hero-slider">
+            <div className="hero-slider-item">
+                <img src={slider_img_1} alt="" />
+            </div>
+        </div>
+    );
 };
 
 export default Slider;
