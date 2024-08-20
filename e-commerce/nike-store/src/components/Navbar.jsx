94d6c4162162757ecf46f533977c18e677f22998
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-08-20 17:45:26 +0700

    feat: add scroll effect and style changes to Navbar component

--- e-commerce/nike-store/src/components/Navbar.jsx
+++ e-commerce/nike-store/src/components/Navbar.jsx
@@ -1,21 +1,49 @@
+import { useState, useEffect } from 'react';
 import { HeartIcon, MagnifyingGlassIcon, ShoppingBagIcon } from '@heroicons/react/24/solid';
 import logo from '../assets/logo.png';
 
 const Navbar = () => {
+    const [navState, setNavState] = useState(false);
+
+    const onNavScroll = () => {
+        if (window.scrollY > 30) {
+            setNavState(true);
+        } else {
+            setNavState(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', onNavScroll);
+
+        return () => {
+            window.removeEventListener('scroll', onNavScroll);
+        };
+    }, []);
     return (
         <>
-            <header className={`absolute left-0 right-0 top-7 z-50 opacity-100`}>
+            <header
+                className={
+                    !navState
+                        ? `absolute left-0 right-0 top-7 z-50 opacity-100`
+                        : 'blur-effect-theme fixed left-0 right-0 top-0 z-[100] flex h-[9vh] items-center justify-center bg-opacity-0'
+                }
+            >
                 <nav className="nike-container flex items-center justify-between">
                     <div className="flex items-center">
-                        <img src={logo} alt="logo/img" className="h-auto w-16" />
+                        <img src={logo} alt="logo/img" className={`h-auto w-16 ${navState && 'brightness-0 filter'}`} />
                     </div>
                     <ul className="flex items-center justify-center gap-2">
                         <li className="grid items-center">
-                            <MagnifyingGlassIcon className="icon-style" />
+                            <MagnifyingGlassIcon
+                                className={`icon-style ${navState && 'text-slate-900 transition-all duration-300'}`}
+                            />
                         </li>
 
                         <li className="grid items-center">
-                            <HeartIcon className="icon-style" />
+                            <HeartIcon
+                                className={`icon-style ${navState && 'text-slate-900 transition-all duration-300'}`}
+                            />
                         </li>
 
                         <li className="grid items-center">
@@ -23,8 +51,12 @@ const Navbar = () => {
                                 type="button"
                                 className="relative border-none outline-none transition-all duration-300 active:scale-110"
                             >
-                                <ShoppingBagIcon className="icon-style" />
-                                <div className="absolute right-0 top-4 flex h-4 w-4 cursor-pointer items-center justify-center rounded-full bg-white text-[0.65rem] font-medium leading-tight text-slate-900 shadow shadow-slate-100 transition-all duration-300 hover:scale-110">
+                                <ShoppingBagIcon
+                                    className={`icon-style ${navState && 'text-slate-900 transition-all duration-300'}`}
+                                />
+                                <div
+                                    className={`absolute right-0 top-4 flex h-4 w-4 cursor-pointer items-center justify-center rounded-full text-[0.65rem] font-medium leading-tight shadow transition-all duration-300 hover:scale-110 ${navState ? 'bg-slate-900 text-slate-100 shadow-slate-900' : 'bg-slate-100 text-slate-900 shadow-slate-100'}`}
+                                >
                                     0
                                 </div>
                             </button>
