eba2a3a7a27fa21a4c063d729b797552b44de235
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-08-21 13:51:18 +0700

    feat: update Navbar Components

--- e-commerce/nike-store/src/components/Navbar.jsx
+++ e-commerce/nike-store/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { HeartIcon, MagnifyingGlassIcon, ShoppingBagIcon } from '@heroicons/react/24/solid';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectTotalQTY, setOpenCart } from '../app/CartSlice.js';
+
+import { HeartIcon, MagnifyingGlassIcon, ShoppingBagIcon } from '@heroicons/react/24/outline';
 import logo from '../assets/logo.png';
-import { setOpenCart } from '../app/CartSlice';
 
 const Navbar = () => {
     const [navState, setNavState] = useState(false);
     const dispatch = useDispatch();
+    const totalQTY = useSelector(selectTotalQTY);
 
     const onCartToggle = () => {
         dispatch(
@@ -23,7 +25,6 @@ const Navbar = () => {
             setNavState(false);
         }
     };
-
     useEffect(() => {
         window.addEventListener('scroll', onNavScroll);
 
@@ -36,8 +37,8 @@ const Navbar = () => {
             <header
                 className={
                     !navState
-                        ? `absolute left-0 right-0 top-7 z-50 opacity-100`
-                        : 'blur-effect-theme fixed left-0 right-0 top-0 z-[100] flex h-[9vh] items-center justify-center bg-opacity-0'
+                        ? 'absolute left-0 right-0 top-7 z-50 opacity-100'
+                        : 'blur-effect-theme fixed left-0 right-0 top-0 z-[200] flex h-[9vh] items-center justify-center opacity-100'
                 }
             >
                 <nav className="nike-container flex items-center justify-between">
@@ -50,13 +51,11 @@ const Navbar = () => {
                                 className={`icon-style ${navState && 'text-slate-900 transition-all duration-300'}`}
                             />
                         </li>
-
                         <li className="grid items-center">
                             <HeartIcon
                                 className={`icon-style ${navState && 'text-slate-900 transition-all duration-300'}`}
                             />
                         </li>
-
                         <li className="grid items-center">
                             <button
                                 type="button"
@@ -69,7 +68,7 @@ const Navbar = () => {
                                 <div
                                     className={`absolute right-0 top-4 flex h-4 w-4 cursor-pointer items-center justify-center rounded-full text-[0.65rem] font-medium leading-tight shadow transition-all duration-300 hover:scale-110 ${navState ? 'bg-slate-900 text-slate-100 shadow-slate-900' : 'bg-slate-100 text-slate-900 shadow-slate-100'}`}
                                 >
-                                    0
+                                    {totalQTY}
                                 </div>
                             </button>
                         </li>
